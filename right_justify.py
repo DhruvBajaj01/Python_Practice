a9ec9179ec25691d03d55050f407a6e2f65f3afe
DhruvBajaj01 <71027506+DhruvBajaj01@users.noreply.github.com>
2021-09-09 12:29:41 +0530

    Add files via upload

---
+++ right_justify.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def right_justify(s):
+    print(' ' * (70-len(s))+s)
+right_justify('allen')    
+right_justify('allen is good')
+
+
+# In[ ]:
+
+
+
+
