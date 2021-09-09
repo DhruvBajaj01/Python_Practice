2bcb501ede976d24c8c25e357d1ec77cb2bab106
DhruvBajaj01 <71027506+DhruvBajaj01@users.noreply.github.com>
2021-09-09 12:31:30 +0530

    Add files via upload

---
+++ Lottery.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+import random
+def Winner():
+     num=random.randint(1,10)
+     return num
+n=int(input("Enter Lottery Number:"))
+if n== Winner():
+       print("Congrats!YOU ARE THE WINNER")
+else :
+   print("Better luck next time")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
