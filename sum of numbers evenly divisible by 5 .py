74c4255dd86f5e28c050ea57cb17028cb65280da
DhruvBajaj01 <71027506+DhruvBajaj01@users.noreply.github.com>
2021-09-09 12:28:47 +0530

    Add files via upload

---
+++ sum of numbers evenly divisible by 5 .py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+sum =0
+a=2
+while a<100 : 
+    print(a)
+    a=a+3
+    if a%5==0 :
+     sum=sum+a
+    
+print("sum of numbers evenly divisible by 5 are ",sum )
+
