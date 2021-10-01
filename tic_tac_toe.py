2d10f4718b996f887b335579a580b25c296ee425
himik-nainwal <59734430+himik-nainwal@users.noreply.github.com>
2021-10-01 15:51:25 +0530

    I made a tic tac toe game using gui in python
    You can play as 2 players I used tkinter module for gui.

---
+++ tic_tac_toe.py
@@ -0,0 +1,62 @@
+from tkinter import *
+
+def callback(r,c):
+  global person
+
+  if person=='x' and states[r][c]==0 and stop== False:
+    b[r][c].configure(text='x', fg='green',bg='black')
+    states[r][c]='x'
+    person='o'
+
+
+  if person=='o' and states[r][c]==0 and stop==False:
+    b[r][c].configure(text='o', fg='orange',bg='light green')
+    states[r][c]='o'
+    person='x'
+    check()
+def check():
+  global stop
+  for i in range(3):
+    if states[i][0]==states[i][1]==states[i][2] !=0:
+      b[i][0].config(bg='grey')
+      b[i][1].config(bg='grey')
+      b[i][2].config(bg='grey')
+      stop=True
+  for i in range(3):
+    if states[0][i]==states[1][i]==states[2][i] !=0:
+      b[0][i].config(bg='grey')
+      b[1][i].config(bg='grey')
+      b[2][i].config(bg='grey')
+      stop=True
+    if states[0][0]==states[1][1]==states[2][2] !=0:
+      b [0][0].config(bg='grey')
+      b [1][1].config(bg='grey')
+      b [2][2].config(bg='grey')
+      stop=True
+    if states[2][0]==states[1][1]==states[0][2] !=0:
+      b[2][0].config(bg='grey')
+      b[1][1].config(bg='grey')
+      b[0][2].config(bg='grey')
+      stop=True
+
+
+root= Tk()
+root.title("Tic Tac Toe")
+
+b=[[0,0,0],
+   [0,0,0],
+   [0,0,0]]
+states=[[0,0,0],
+   [0,0,0],
+   [0,0,0]]
+
+
+for i in range(3):
+  for j in range (3):
+    b[i][j]=Button(font=("Arial",80),width=4,bg='green',
+    command= lambda r=i , c=j: callback(r,c))
+    b[i][j].grid(row=i,column=j)
+person='x'
+stop=False
+
+mainloop()
