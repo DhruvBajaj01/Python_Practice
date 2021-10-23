5e1953af7894b04a639fb4616cb68e711e804d84
sanjanapaidi <70941068+sanjanapaidi@users.noreply.github.com>
2021-10-23 22:09:28 +0530

    Added Hollow Diamond

---
+++ HollowDiamond.py
@@ -0,0 +1,19 @@
+row = int(input())
+for i in range(1, row+1):
+    for j in range(1,row-i+1):
+        print(" ", end="")
+    for j in range(1, 2*i):
+        if j==1 or j==2*i-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+for i in range(row-1,0, -1):
+    for j in range(1,row-i+1):
+        print(" ", end="")
+    for j in range(1, 2*i):
+        if j==1 or j==2*i-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
